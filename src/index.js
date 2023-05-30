fe06a8c2cf33e99ac2fcdfdbc7aa5b9d7bac9128
Gerardo Crot Rojas <gicrot@uc.cl>
2023-05-29 22:00:13 -0400

    feat: new relic integration with node

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const newrelic = require("newrelic");
+
+const app = express();
+
+dotenv.config();
+
+app.set("port", process.env.PORT || 3000);
+app.set("json spaces", 2);
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    Title: "Hello World to my monitored api",
+  });
+});
+
+app.get("/ping", (req, res) => {
+  newrelic.incrementMetric('Ping/NumberOfCalls', 1);
+  res.send("Pong");
+});
+
+//Iniciando el servidor, escuchando...
+app.listen(app.get('port'),()=>{
+  console.log(`Server listening on port ${app.get('port')}`);
+});
